12d5443a6b03ee047fff8977325245deb44c4164
Alberto Martinez de la Ossa <delaossa@mail.desy.de>
2016-08-04 22:15:27 +0200

    Trying to figure out how to change the indexing of the data

--- plotCharge.py
+++ plotCharge.py
@@ -1,31 +1,36 @@
 from __future__ import print_function
 import h5py
 import numpy as np
-import vtk
+from vtk import *
 
 #hf = h5py.File('data/charge-beam-driver-000026.h5','r')
-hf = h5py.File('/afs/desy.de/group/fla/plasma/data002/OSIRIS-runs/3D-runs/RAKE/rake-v10kA.G.SR2.RI.3D/MS/DENSITY/beam-driver/charge/charge-beam-driver-000026.h5','r')
+hf = h5py.File('/home/delaossa/data/charge-beam-driver-000026.h5','r')
 
 data = hf.get('charge')
-print('Shape of the array charge: ', data.shape,'\nType: ',data.dtype,'\n')
+axisz = hf.get('AXIS/AXIS1')
+axisy = hf.get('AXIS/AXIS2')
+axisx = hf.get('AXIS/AXIS3')
+
+dz = (axisz[1]-axisz[0])/data.shape[2]
+dy = (axisy[1]-axisy[0])/data.shape[1]
+dx = (axisx[1]-axisx[0])/data.shape[0]
+
+# This is for the spacing
+skn = 10.0
+
+print('Axis z range: [%.2f,%.2f]  Nbins = %i  dz = %.4f' % (axisz[0],axisz[1],data.shape[2],dz) )
+print('Axis x range: [%.2f,%.2f]  Nbins = %i  dx = %.4f' % (axisx[0],axisx[1],data.shape[0],dx) )
+print('Axis y range: [%.2f,%.2f]  Nbins = %i  dy = %.4f' % (axisy[0],axisy[1],data.shape[1],dy) )
+
+print('Shape of the array: ', data.shape,'\nType: ',data.dtype,'\n')
 
 # Changing to positive integer types (particle density)
 # it is required by vtkVolumeRayCastMapper
 npdata = np.array(data)
 npdata = -100 * npdata
 npdataint = np.array(npdata, dtype=np.uint8)
-#npdataint = np.zeros(npdata.shape, dtype=np.uint8)
-#npdataint = np.rint(npdata)
-#npdataint.astype(np.uint8)
 
-print('Shape of the array charge: ', npdataint.shape,'\nType: ',npdataint.dtype,'\n')
-
-#print('Converting data...')
-# This is very slow
-#for i in range(0, data.shape[0]):
-#    for j in range(0, data.shape[1]):
-#        for k in range(0, data.shape[2]):
-#            npdataint[i,j,k] = -10 * npdata[i,j,k]
+print('Shape of the array: ', npdataint.shape,'\nType: ',npdataint.dtype,'\n')
 
 print('Rendering...')
             
@@ -53,10 +58,33 @@ dataImporter.SetNumberOfScalarComponents(1)
 # and the dimensions of the array it is stored in.
 dataImporter.SetDataExtent(0, npdataint.shape[0]-1, 0, npdataint.shape[1]-1, 0, npdataint.shape[2]-1)
 dataImporter.SetWholeExtent(0, npdataint.shape[0]-1, 0, npdataint.shape[1]-1, 0, npdataint.shape[2]-1)
+dataImporter.SetDataSpacing(skn*dx,skn*dy,skn*dz)
+dataImporter.SetDataOrigin(skn*axisx[0],skn*axisy[0],0.0)
+
+# Operations on the data
+imageInt = vtk.vtkImageCast()
+imageInt.SetInputConnection(dataImporter.GetOutputPort())
+imageInt.SetOutputScalarTypeToUnsignedChar()
+
+flipX = vtk.vtkImageFlip()
+flipX.SetInputConnection(imageInt.GetOutputPort())
+flipX.SetFilteredAxis(0)
+
+flipY = vtk.vtkImageFlip()
+flipY.SetInputConnection(imageInt.GetOutputPort())
+flipY.SetFilteredAxis(1)
+flipY.FlipAboutOriginOn()
+
+imageAppend = vtk.vtkImageAppend()
+imageAppend.AddInputConnection(imageInt.GetOutputPort())
+#imageAppend.AddInputConnection(flipX.GetOutputPort())
+#imageAppend.AddInputConnection(flipY.GetOutputPort())
+imageAppend.SetAppendAxis(0)
+
 
 alphaChannelFunc = vtk.vtkPiecewiseFunction()
 alphaChannelFunc.AddPoint(0, 0.0)
-alphaChannelFunc.AddPoint(11, 0.0)
+alphaChannelFunc.AddPoint(0.02*maxvalue, 0.0)
 alphaChannelFunc.AddPoint(maxvalue, 0.8)
 
 # This class stores color data and can create color tables from a few color points.
@@ -79,7 +107,7 @@ compositeFunction = vtk.vtkVolumeRayCastCompositeFunction()
 # We can finally create our volume. We also have to specify the data for it, as well as how the data will be rendered.
 volumeMapper = vtk.vtkVolumeRayCastMapper()
 volumeMapper.SetVolumeRayCastFunction(compositeFunction)
-volumeMapper.SetInputConnection(dataImporter.GetOutputPort())
+volumeMapper.SetInputConnection(imageAppend.GetOutputPort())
 
 # The class vtkVolume is used to pair the previously declared volume as well as the properties to be used when rendering that volume.
 volume = vtk.vtkVolume()
@@ -88,22 +116,66 @@ volume.SetProperty(volumeProperty)
 
 # With almost everything else ready, its time to initialize the renderer and window
 renderer = vtk.vtkRenderer()
-renderWin = vtk.vtkRenderWindow()
-renderWin.AddRenderer(renderer)
-renderInteractor = vtk.vtkRenderWindowInteractor()
-renderInteractor.SetRenderWindow(renderWin)
-
-# We add the volume to the renderer ...
-renderer.AddVolume(volume)
-
 # ... set background color to white ...
 renderer.SetBackground(0,0,0)
 # Other colors 
 # nc = vtk.vtkNamedColors()
 # renderer.SetBackground(nc.GetColor3d('MidnightBlue'))
 
-# ... and set window size.
-renderWin.SetSize(800, 600)
+# We add the volume to the renderer ...
+renderer.AddVolume(volume)
 
-renderInteractor.Initialize()
-renderInteractor.Start()
+# Adding the scalar bar color palette
+scalarBar = vtkScalarBarActor()
+scalarBar.SetTitle("Density")
+scalarBar.SetLookupTable(colorFunc);
+scalarBar.SetOrientationToVertical();
+scalarBar.SetPosition( 0.85, 0.7 );
+scalarBar.SetPosition2( 0.1, 0.3 );
+propT = vtkTextProperty()
+propL = vtkTextProperty()
+propT.SetFontFamilyToArial()
+propT.ItalicOff()
+propT.BoldOn()
+propL.BoldOff()
+scalarBar.SetTitleTextProperty(propT);
+scalarBar.SetLabelTextProperty(propL);
+scalarBar.SetLabelFormat("%5.2f")
+
+renderer.AddActor(scalarBar)
+
+ 
+axes = vtk.vtkAxesActor()
+axes.SetShaftTypeToLine()
+axes.SetTotalLength(skn*(axisx[1]-axisx[0])/2.0,skn*(axisy[1]-axisy[0])/2.0,skn*(axisz[1]-axisz[0])/2.0)
+#axes.SetNormalizedShaftLength(1.0, 1.0, 1.0)
+#axes.SetNormalizedTipLength(0.05, 0.05, 0.05)
+# The axes are positioned with a user transform
+transform = vtk.vtkTransform()
+transform.Translate(0.0, 0.0, 0.0)
+axes.SetUserTransform(transform)
+ 
+# properties of the axes labels can be set as follows
+# this sets the x axis label to red
+#axes.GetXAxisCaptionActor2D().GetCaptionTextProperty().SetColor(1,0,0);
+ 
+# the actual text of the axis label can be changed:
+# axes.SetXAxisLabelText("x");
+ 
+renderer.AddActor(axes)
+
+ 
+
+window = vtk.vtkRenderWindow()
+window.AddRenderer(renderer)
+# ... and set window size.
+window.SetSize(800, 600)
+
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(window)
+#style = vtkInteractorStyleTrackballCamera();
+#interactor.SetInteractorStyle(style);
+
+
+interactor.Initialize()
+interactor.Start()
